cd451a28a7cf15c1b567a314456cab61910a25b8
Eric <ekellogg90@gmail.com>
2024-07-05 16:12:43 -0500

    added seed data for tasks, users, and added execution code into seed.js

--- seeds/tasks-seeds.js
+++ seeds/tasks-seeds.js
@@ -1,31 +1,48 @@
-const tagData = [
+const { Task } = require('../models');
+const taskData = [
     {
-      tag_name: 'Groceries',
+      task_name: 'Groceries', 
+      category_id: 1,
+      priority_id: 1
     },
     {
       tag_name: 'Clean',
+      category_id: 1,
+      priority_id: 2
     },
     {
       tag_name: 'Maintenance',
+      category_id: 2,
+      priority_id: 3
     },
     {
       tag_name: 'Laundry',
+      category_id: 1,
+      priority_id: 1
     },
     {
       tag_name: 'Feed Pets',
+      category_id: 5,
+      priority_id: 2
     },
     {
       tag_name: 'Mow',
+      category_id: 3,
+      priority_id: 3
     },
     {
       tag_name: 'Make Dinner',
+      category_id: 5,
+      priority_id: 1
     },
     {
       tag_name: 'Walk the dog',
+      category_id: 1,
+      priority_id: 2
     },
   ];
   
-  const seedTags = () => Tag.bulkCreate(tagData);
+  const seedTasks = () => Task.bulkCreate(taskData);
   
-  module.exports = seedTags;
+  module.exports = seedTasks;
   
