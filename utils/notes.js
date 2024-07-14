156ecd6551145cd06470841a62240e05d0e55d32
Michael Hellon <kansasboy308@gmail.com>
2024-07-14 18:15:06 -0500

    added dayjs to notes.js Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   utils/notes.js

---
+++ utils/notes.js
@@ -0,0 +1,7 @@
+const dayjs = require("dayjs");
+
+// gets full year, month, day, hour, min, sec 
+const currentDate = new Date();
+const formattedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')} ${currentDate.getHours().toString().padStart(2, '0')}:${currentDate.getMinutes().toString().padStart(2, '0')}:${currentDate.getSeconds().toString().padStart(2, '0')}`;
+
+console.log('current date time:', formattedDate)
