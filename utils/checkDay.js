4584058dd76658aa0f3a334424ffd242834b2399
NoahCalderwood <noahcalderwood3@gmail.com>
2024-07-14 21:40:03 -0500

    added checkboxes to profile forms

---
+++ utils/checkDay.js
@@ -0,0 +1,9 @@
+let Monday = document.getElementById('Monday');
+let Tuesday = document.getElementById('Tuesday');
+let Wednesday = document.getElementById('Wednesday');
+let Thursday = document.getElementById('Thursday');
+let Friday = document.getElementById('Friday');
+let Saturday = document.getElementById('Saturday');
+let Sunday = document.getElementById('Sunday');
+
+console.log(Monday);
